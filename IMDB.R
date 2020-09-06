8107d89ae67f9787ae68f4bf508749382baf6cde
andersonb97 <56001037+andersonb97@users.noreply.github.com>
2020-09-06 14:44:50 -0600

    Preliminary work on genres and keywords

--- IMDB.R
+++ IMDB.R
@@ -8,7 +8,12 @@ library(ggplot2)
 
 ## Read in the data
 imdb.test <- read_csv("IMDBTest.csv")
-imdb <- read_csv("IMDBTrain.csv")
+imdb.train <- read_csv("IMDBTrain.csv")
+
+## Merge the two datasets together so when I clean the 
+## training dataset I also treat teh test set the same way
+names(imdb.test)[names(imdb.test) == "ID"] <- "movie_title"
+imdb <- bind_rows(train = imdb.train, test = imdb.test, .id = "Set")
 
 ##
 ## Exploratory Data Analysis
@@ -17,22 +22,22 @@ imdb <- read_csv("IMDBTrain.csv")
 ## Scatterplot of Budget vs. Score
 ggplot(data = imdb, mapping = aes(x = budget, y = imdb_score)) +
   geom_point()
-imdb %>% filter(budget > 100000000, country == "USA") %>% 
+imdb.train %>% filter(budget > 100000000, country == "USA") %>% 
   select(movie_title)
 
 ## Missing Values (what do you do?)
 # Delete, Fill (Mean, Median, Regression), or Ignore
 ## Scatterplot of groww vs imdb
-ggplot(data = imdb, mapping = aes(x = gross, y = imdb_score)) +
+ggplot(data = imdb.train, mapping = aes(x = gross, y = imdb_score)) +
   geom_point()
-with(imdb, cor(gross, imdb_score, use = "complete.obs"))
+with(imdb.train, cor(gross, imdb_score, use = "complete.obs"))
 
 
 ## Tasks
 ## Convert data to US dollars
 
 ## Analyze my Columns
-my.col <- imdb[, 13:16]
+my.col <- imdb.train[, 13:16]
 names(my.col)
 head(my.col)
 
@@ -41,15 +46,15 @@ head(my.col)
 #   geom_boxplot()
 # ggplot(data = imdb, mapping = aes(y = cast_total_facebook_likes))+
 #   geom_boxplot()
-ggplot(data = imdb, mapping = aes(y = facenumber_in_poster))+
+ggplot(data = imdb.train, mapping = aes(y = facenumber_in_poster))+
   geom_boxplot()
 
 ## Note both are skewed right
-# ggplot(data = imdb, mapping = aes(x = num_voted_users))+
+# ggplot(data = imdb.train, mapping = aes(x = num_voted_users))+
 #   geom_histogram()
-# ggplot(data = imdb, mapping = aes(x = cast_total_facebook_likes))+
+# ggplot(data = imdb.train, mapping = aes(x = cast_total_facebook_likes))+
 #   geom_histogram()
-# ggplot(data = imdb, mapping = aes(x = facenumber_in_poster))+
+# ggplot(data = imdb.train, mapping = aes(x = facenumber_in_poster))+
 #   geom_histogram()
 
 ## How many Na's are there in all of the columns
@@ -64,10 +69,33 @@ is.na(my.col) %>% apply(2, sum)
 ## to fill those in with 0's because there technically were no faces on
 ## the poster.
 
-head(imdb[, 17:28])
-names(imdb[, 17:28])
+head(imdb.train[, 17:28])
+names(imdb.train[, 17:28])
 
-imdb[which(is.na(my.col[, 3:4])),]$country
+imdb.train[which(is.na(my.col[, 3:4])),]$country
 
-imdb %>% filter(is.na(facenumber_in_poster)) %>% 
+imdb.train %>% filter(is.na(facenumber_in_poster)) %>% 
   select(movie_title)
+
+############## In class Cloumns ############
+new.col <- imdb.train[, c("genres", "plot_keywords")]
+
+is.na(new.col) %>% apply(2, sum)
+
+##### KEYWORDS
+imdb$plot_keywords %>% sapply(function(x){unlist(str_split(x, "\\|"))})
+
+words <- imdb$plot_keywords %>% sapply(function(x){unlist(str_split(x, "[\\| ]"))}) %>% 
+  unlist() %>% unname() %>% table() %>%  sort(decreasing = TRUE)
+
+#list out words and frequency
+stopwords::stopwords("english")
+
+which()
+
+##### Genres
+imdb$genres %>% sapply(function(x){unlist(str_split(x, "[\\| ]"))}) %>% 
+  unlist() %>% unname() %>% table() %>%  sort(decreasing = TRUE) 
+
+imdb$genres %>% sapply(function(x){unlist(str_split(x, "[\\| ]"))}) %>% 
+   qdapTools::mtabulate()
